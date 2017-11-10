fc3ab2e81306b35e6de4abdf9fa702deedc442cb
Dave Cheney <dave@cheney.net>
2017-11-10 22:19:36 +1100

    Initial import

---
+++ examples/fib/fib_test.go
@@ -0,0 +1,37 @@
+package fib
+
+// STARTBENCH OMIT
+import "testing"
+
+// STARTFIB OMIT
+// Fib computes the n'th number in the Fibonacci series.
+func Fib(n int) int {
+	switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		return Fib(n-1) + Fib(n-2)
+	}
+}
+
+// ENDFIB OMIT
+
+func BenchmarkFib(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Fib(20) // run the Fib function b.N times
+	}
+}
+
+// ENDBENCH OMIT
+
+func TestFib(t *testing.T) {
+	fibs := []int{0, 1, 1, 2, 3, 5, 8, 13, 21}
+	for n, want := range fibs {
+		got := Fib(n)
+		if want != got {
+			t.Errorf("Fib(%d): want %d, got %d", n, want, got)
+		}
+	}
+}
